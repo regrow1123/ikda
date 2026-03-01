811734a77c540c66ba9293e7f5c110afb6267843
seongje.jang <tjdwp90@snu.ac.kr>
2026-03-01 14:54:18 +0900

    feat: 스크랩 Supabase DB 연동 (CRUD)

--- lib/presentation/screens/highlight/highlight_detail_screen.dart
+++ lib/presentation/screens/highlight/highlight_detail_screen.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../providers/highlight_provider.dart';
-import '../../../data/models/highlight.dart';
 
 class HighlightDetailScreen extends ConsumerWidget {
   final int highlightId;
